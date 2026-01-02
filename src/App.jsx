72e51a2aea394835069bc0fe6542c7e27033a1c8
muhammadamin2025 <muhammadaminjs@icloud.com>
2026-01-02 15:33:50 +0500

    Baglar to'g'irlandi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 function App() {
   return (
-    <h1 className="text-4xl font-bold text-red-600">Tailwind ishlayapti 🔥</h1>
+    <>
+      <h1 className="text-4xl font-bold text-red-600">
+        Tailwind ishlayapti 🔥
+      </h1>
+    </>
   );
 }
 
